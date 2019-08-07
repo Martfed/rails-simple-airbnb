7c42db261eda23f62065eb5465273998d14e5c39
Federico De Martino <federicodm90@gmail.com>
2019-08-07 15:50:00 +0200

    without styyyle

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  flats = Flat.new(
+    name: Faker::Movies::LordOfTheRings.character,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: Faker::Number.decimal_part(digits: 2),
+    number_of_guests: Faker::Number.decimal_part(digits: 2)
+  )
+  flats.save!
+end
